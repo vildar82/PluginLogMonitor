8d52526cb62fbc84b87689eff2baf6c449d0f984
Vildar Khisyametdinov <vildar82@gmail.com>
2016-04-09 16:23:26 +0300

    Версия среды .NET Framework

--- LogMonitor/Core/AllUsers/MonitorAcadUsers.cs
+++ LogMonitor/Core/AllUsers/MonitorAcadUsers.cs
@@ -6,197 +6,221 @@ using System.Text;
 
 namespace LogMonitor.Core.AllUsers
 {
-   /// <summary>
-   /// Парсер логов AutoCAD_PIK_Manager
-   /// </summary>
-   public class MonitorAcadUsers
-   {
-      public string Report { get; private set; } = string.Empty;
-      public string LogFolder { get; set; }  = @"\\dsk2.picompany.ru\project\CAD_Settings\AutoCAD_server\ShareSettings\AutoCAD_PIK_Manager\Logs";
-      public int DaysLookingFor { get; set; } = 31;
-      public string GroupAD { get; set; } = "adm-dsk3-AutoCADSettings-u";
+    /// <summary>
+    /// Парсер логов AutoCAD_PIK_Manager
+    /// </summary>
+    public class MonitorAcadUsers
+    {
+        public string Report { get; private set; } = string.Empty;
+        public string LogFolder { get; set; } = @"\\dsk2.picompany.ru\project\CAD_Settings\AutoCAD_server\ShareSettings\AutoCAD_PIK_Manager\Logs";
+        public int DaysLookingFor { get; set; } = 31;
+        public string GroupAD { get; set; } = "adm-dsk3-AutoCADSettings-u";
 
-      // Все пользователи в группе AD
-      public List<UserInfo> UsersAD { get; private set; } = new List<UserInfo>();
+        // Все пользователи в группе AD
+        public List<UserInfo> UsersAD { get; private set; } = new List<UserInfo>();
 
-      // Пользователи с ошибками в логе
-      public List<UserInfo> UsersErorInLog { get; private set; } = new List<UserInfo>();
+        // Пользователи с ошибками в логе
+        public List<UserInfo> UsersErorInLog { get; private set; } = new List<UserInfo>();
 
-      // Пользователи которых нет в группе АД, но есть в логах
-      public List<UserInfo> UsersErorInLogNotInAD { get; private set; } = new List<UserInfo>();
+        // Пользователи которых нет в группе АД, но есть в логах
+        public List<UserInfo> UsersErorInLogNotInAD { get; private set; } = new List<UserInfo>();
 
-      // Пользователи которых нет в логах
-      public List<UserInfo> UsersErorNotInLog { get; private set; } = new List<UserInfo>();
+        // Пользователи которых нет в логах
+        public List<UserInfo> UsersErorNotInLog { get; private set; } = new List<UserInfo>();
 
-      // Пользователи у которых давно нет успешной установки настроек
-      public List<UserInfo> UsersErorLongSuccess { get; private set; } = new List<UserInfo>();
+        // Пользователи у которых давно нет успешной установки настроек
+        public List<UserInfo> UsersErorLongSuccess { get; private set; } = new List<UserInfo>();
 
-      // Пользователи в логах у которых все ОК
-      public List<UserInfo> UsersLog { get; private set; } = new List<UserInfo>();
+        // Пользователи в логах у которых все ОК
+        public List<UserInfo> UsersLog { get; private set; } = new List<UserInfo>();
 
-      public void CheckAllUsers()
-      {
-         Parse();
-         SendReport();
-      }
+        public void CheckAllUsers()
+        {
+            Parse();
+            SendReport();
+        }
 
-      private void Parse()
-      {
-         UsersAD = ADUtils.GetUsersInGroup(GroupAD);
-         var logFiles = Directory.GetFiles(LogFolder, "*.log", SearchOption.TopDirectoryOnly);
-         // Поиск пользователей в логах
-         foreach (var log in logFiles)
-         {
-            getUserInLog(log);
-         }
-         // Проверка пользователей
-         CheckUsersInLog();
-      }
-
-      private void SendReport()
-      {
-         //Отправка отчета
-         Report = ReportAllUsers.GetReportMessage(this);
-         EmailLog.SendEmail(Report, "Log Monitor All Users AutoCAD Settings");
-      }
-
-      private static void findLastError(UserInfo userlog, IEnumerable<string> lines)
-      {
-         foreach (var line in lines)
-         {
-            var logType = line.Split('_');
-            if (logType.Length > 1)
+        private void Parse()
+        {
+            UsersAD = ADUtils.GetUsersInGroup(GroupAD);
+            var logFiles = Directory.GetFiles(LogFolder, "*.log", SearchOption.TopDirectoryOnly);
+            // Поиск пользователей в логах
+            foreach (var log in logFiles)
             {
-               if (logType[1].StartsWith("Error"))
-               {
-                  userlog.LastError += line;
-                  break;
-               }
+                getUserInLog(log);
             }
-         }
-      }
+            // Проверка пользователей
+            CheckUsersInLog();
+        }
 
-      private static void findUserGroupAcad(UserInfo userlog, IEnumerable<string> lines)
-      {
-         foreach (var line in lines)
-         {
-            string valueSearch = string.Format("{0} Группа - ", userlog.Login);
-            var index = line.IndexOf(valueSearch, StringComparison.OrdinalIgnoreCase);
-            if (index> 0)
+        private void SendReport()
+        {
+            //Отправка отчета
+            Report = ReportAllUsers.GetReportMessage(this);
+            EmailLog.SendEmail(Report, "Log Monitor All Users AutoCAD Settings");
+        }
+
+        private static void findLastError(UserInfo userlog, IEnumerable<string> lines)
+        {
+            foreach (var line in lines)
             {
-               userlog.GroupAcad = line.Substring(index + valueSearch.Length).Trim();
+                var logType = line.Split('_');
+                if (logType.Length > 1)
+                {
+                    if (logType[1].StartsWith("Error"))
+                    {
+                        userlog.LastError += line;
+                        break;
+                    }
+                }
             }
-         }
-      }
+        }
 
-      private void findLastSuccessSetting(UserInfo userlog, IEnumerable<string> lines)
-      {
-         foreach (var line in lines)
-         {
-            if (line.Contains("Профиль ПИК установлен"))
+        private static void findUserGroupAcad(UserInfo userlog, IEnumerable<string> lines)
+        {
+            foreach (var line in lines)
             {
-               var logType = line.Split('_');
-               if (logType.Length > 1)
-               {
-                  var lastSuccesSetting = DateTime.Parse(logType[0]);
-                  if (lastSuccesSetting > userlog.LastSuccesSetting)
-                  {
-                     userlog.LastSuccesSetting = lastSuccesSetting;
-                  }
-                  break;
-               }
+                string valueSearch = string.Format("{0} Группа - ", userlog.Login);
+                var index = line.IndexOf(valueSearch, StringComparison.OrdinalIgnoreCase);
+                if (index > 0)
+                {
+                    userlog.GroupAcad = line.Substring(index + valueSearch.Length).Trim();
+                }
             }
-         }
-      }
+        }
 
-      private List<UserInfo> checkUserNotLog()
-      {
-         List<UserInfo> checkUserNotLog = new List<UserInfo>();
-         foreach (var userAd in UsersAD)
-         {
-            if (!UsersLog.Exists(u => isEqualLogins(u.Login, userAd.Login)))
+        private void findLastSuccessSetting(UserInfo userlog, IEnumerable<string> lines)
+        {
+            foreach (var line in lines)
             {
-               checkUserNotLog.Add(userAd);
+                if (line.Contains("Профиль ПИК установлен"))
+                {
+                    var logType = line.Split('_');
+                    if (logType.Length > 1)
+                    {
+                        var lastSuccesSetting = DateTime.Parse(logType[0]);
+                        if (lastSuccesSetting > userlog.LastSuccesSetting)
+                        {
+                            userlog.LastSuccesSetting = lastSuccesSetting;
+                        }
+                        break;
+                    }
+                }
             }
-         }
-         return checkUserNotLog;
-      }
+        }
 
-      private void CheckUsersInLog()
-      {
-         UsersErorNotInLog = checkUserNotLog();
-
-         foreach (var userLog in UsersLog)
-         {            
-            if (userLog.LastSuccesSetting == DateTime.MinValue)
+        private List<UserInfo> checkUserNotLog()
+        {
+            List<UserInfo> checkUserNotLog = new List<UserInfo>();
+            foreach (var userAd in UsersAD)
             {
-               // Нет успешного завершения настроек
-               UsersErorInLog.Add(userLog);
+                if (!UsersLog.Exists(u => isEqualLogins(u.Login, userAd.Login)))
+                {
+                    checkUserNotLog.Add(userAd);
+                }
             }
-            else
-            {
-               // Давно нет успешной установки настроек
-               var daysForLastSucces = (DateTime.Now - userLog.LastSuccesSetting).Days;
-               if (daysForLastSucces > DaysLookingFor)
-               {
-                  UsersErorLongSuccess.Add(userLog);
-               }
-            }
-         }
-         UsersErorInLog.ForEach(e => UsersLog.Remove(e));
-         UsersErorLongSuccess.ForEach(e => UsersLog.Remove(e));
-      }
+            return checkUserNotLog;
+        }
 
-      private string getLoginByFileLogNam(string log)
-      {
-         string res = Path.GetFileNameWithoutExtension(log);
-         int indexC = res.IndexOf("-C-"); // англ C
-         if (indexC == -1)
-         {
-            indexC = res.IndexOf("-С-"); // рус С
+        private void CheckUsersInLog()
+        {
+            UsersErorNotInLog = checkUserNotLog();
+
+            foreach (var userLog in UsersLog)
+            {
+                if (userLog.LastSuccesSetting == DateTime.MinValue)
+                {
+                    // Нет успешного завершения настроек
+                    UsersErorInLog.Add(userLog);
+                }
+                else
+                {
+                    // Давно нет успешной установки настроек
+                    var daysForLastSucces = (DateTime.Now - userLog.LastSuccesSetting).Days;
+                    if (daysForLastSucces > DaysLookingFor)
+                    {
+                        UsersErorLongSuccess.Add(userLog);
+                    }
+                }
+            }
+            UsersErorInLog.ForEach(e => UsersLog.Remove(e));
+            UsersErorLongSuccess.ForEach(e => UsersLog.Remove(e));
+        }
+
+        private string getLoginByFileLogNam(string log)
+        {
+            string res = Path.GetFileNameWithoutExtension(log);
+            int indexC = res.IndexOf("-C-"); // англ C
             if (indexC == -1)
             {
-               return res;
+                indexC = res.IndexOf("-С-"); // рус С
+                if (indexC == -1)
+                {
+                    return res;
+                }
             }
-         }
-         return res.Substring(0, indexC);
-      }
+            return res.Substring(0, indexC);
+        }
 
-      private void getUserInLog(string log)
-      {
-         // Определение пользователя по имени лог файла
-         string loginByLog = getLoginByFileLogNam(log);
-         UserInfo userlog = UsersLog.Find(u => isEqualLogins(loginByLog, u.Login));
-         if (userlog == null)
-         {
-            userlog = new UserInfo(loginByLog, loginByLog, "");
-            UsersLog.Add(userlog);
-            // Поиск пользователя в списке пользователей AD
-            var userAD = UsersAD.Find(u => isEqualLogins(loginByLog, u.Login));
-            if (userAD == null)
+        private void getUserInLog(string log)
+        {
+            // Определение пользователя по имени лог файла
+            string loginByLog = getLoginByFileLogNam(log);
+            UserInfo userlog = UsersLog.Find(u => isEqualLogins(loginByLog, u.Login));
+            if (userlog == null)
             {
-               // Пользователь которого нет в списке группы АД - возможно это старый пользователь, которого уволили или типа того.
-               UsersErorInLogNotInAD.Add(userlog);
+                userlog = new UserInfo(loginByLog, loginByLog, "");
+                UsersLog.Add(userlog);
+                // Поиск пользователя в списке пользователей AD
+                var userAD = UsersAD.Find(u => isEqualLogins(loginByLog, u.Login));
+                if (userAD == null)
+                {
+                    // Пользователь которого нет в списке группы АД - возможно это старый пользователь, которого уволили или типа того.
+                    UsersErorInLogNotInAD.Add(userlog);
+                }
+                else
+                {
+                    userlog.Name = userAD.Name;
+                    userlog.Login = userAD.Login;
+                    userlog.GroupAD = userAD.GroupAD;
+                }
             }
-            else
-            {
-               userlog.Name = userAD.Name;
-               userlog.Login = userAD.Login;
-               userlog.GroupAD = userAD.GroupAD;
-            }
-         }
-         var lines = File.ReadAllLines(log, Encoding.Default).Reverse();
-         // поиск строки успешного выполнения настроек - "Профиль ПИК установлен."
-         findLastSuccessSetting(userlog, lines);
-         // Поиск последней ошибки
-         findLastError(userlog, lines);
-         // Поиск группы AutoCAD (шифр отдела)
-         findUserGroupAcad(userlog, lines);
-      }
+            var lines = File.ReadAllLines(log, Encoding.Default).Reverse();
+            // поиск строки успешного выполнения настроек - "Профиль ПИК установлен."
+            findLastSuccessSetting(userlog, lines);
+            // Поиск последней ошибки
+            findLastError(userlog, lines);
+            // Поиск группы AutoCAD (шифр отдела)
+            findUserGroupAcad(userlog, lines);
+            // Версия Net Framework
+            findNetVersion(userlog, lines);
+        }
 
-      private bool isEqualLogins(string loginByLog, string login)
-      {
-         return loginByLog.IndexOf(login, StringComparison.OrdinalIgnoreCase) >= 0;
-      }
-   }
+        private void findNetVersion(UserInfo userlog, IEnumerable<string> lines)
+        {
+            foreach (var line in lines)
+            {
+                string searchInput = "Версия среды .NET Framework -";
+                var index = line.IndexOf(searchInput, StringComparison.OrdinalIgnoreCase);
+                if (index > 0)
+                {                    
+                    var value = line.Substring(index + searchInput.Length).Trim();
+                    try
+                    {
+                        Version ver = Version.Parse(value);
+                        if (userlog.NetVersion != null && userlog.NetVersion < ver)
+                        {
+                            userlog.NetVersion = ver;
+                        }
+                    }
+                    catch { }
+                }
+            }
+        }
+
+        private bool isEqualLogins(string loginByLog, string login)
+        {
+            return loginByLog.IndexOf(login, StringComparison.OrdinalIgnoreCase) >= 0;
+        }
+    }
 }
--- LogMonitor/Core/AllUsers/ReportAllUsers.cs
+++ LogMonitor/Core/AllUsers/ReportAllUsers.cs
@@ -6,43 +6,50 @@ using System.Threading.Tasks;
 
 namespace LogMonitor.Core.AllUsers
 {
-   public static class ReportAllUsers
-   {
-      public static string GetReportMessage(MonitorAcadUsers apm)
-      {
-         StringBuilder report = new StringBuilder("Отчет по всем пользователям настроек AutoCAD\n");
+    public static class ReportAllUsers
+    {
+        public static string GetReportMessage(MonitorAcadUsers apm)
+        {
+            StringBuilder report = new StringBuilder("Отчет по всем пользователям настроек AutoCAD\n");
 
-         report.AppendFormat("\nГруппа AD: {0}, Папка логов: {1}\n", apm.GroupAD, apm.LogFolder);
-         report.AppendFormat("\nUsersAD Пользователей в группе AD настроек AutoCAD: {0}", apm.UsersAD.Count);
-         report.AppendFormat("\nUsersLog Пользователей в логах у которых все ок: {0}", apm.UsersLog.Count);
-         report.AppendLine("\n\nПользователей с ошибками");
-         report.AppendFormat("\nUsersErorNotInLog Пользователей которых нет в логах (не произошла настройка вообще): {0}",
-                              apm.UsersErorNotInLog.Count);
-         report.AppendFormat("\nUsersErorInLog Пользователей с ошибками в логе: {0}", apm.UsersErorInLog.Count);
-         report.AppendFormat("\nUsersErorInLogNotInADПользователей которых нет в AD но есть логи (уволенные или типа того): {0}",
-                              apm.UsersErorInLogNotInAD.Count);
-         report.AppendFormat("\nUsersErorLongSuccessПользователей у которых давно не выполнялась настройка (может комп сменился или типа того): {0}",
-                              apm.UsersErorLongSuccess.Count);
-         report.AppendLine("\nПодробнее:");
-         report.Append(getReportForUsers(apm.UsersErorNotInLog, "UsersErorNotInLog - которых нет в логах (не произошла настройка вообще)\n"));
-         report.Append(getReportForUsers(apm.UsersErorInLog, "UsersErorInLog Пользователей с ошибками в логе\n"));
-         report.Append(getReportForUsers(apm.UsersErorInLogNotInAD, "UsersErorInLogNotInADПользователей которых нет в AD но есть логи (уволенные или типа того)\n"));
-         report.Append(getReportForUsers(apm.UsersErorLongSuccess, "UsersErorLongSuccessПользователей у которых давно не выполнялась настройка (может комп сменился или типа того)\n"));
+            report.AppendFormat($"\n:Версии .NET Framework:");
+            var netVers = apm.UsersLog.GroupBy(u => u.NetVersion).OrderByDescending(g => g.Count());            
+            foreach (var ver in netVers)
+            {
+                report.AppendFormat($"\n\t{ver.Key} - {ver.Count()}");
+            }
 
-         return report.ToString();
-      }
+            report.AppendFormat("\nГруппа AD: {0}, Папка логов: {1}\n", apm.GroupAD, apm.LogFolder);
+            report.AppendFormat("\nUsersAD Пользователей в группе AD настроек AutoCAD: {0}", apm.UsersAD.Count);
+            report.AppendFormat("\nUsersLog Пользователей в логах у которых все ок: {0}", apm.UsersLog.Count);
+            report.AppendLine("\n\nПользователей с ошибками");
+            report.AppendFormat("\nUsersErorNotInLog Пользователей которых нет в логах (не произошла настройка вообще): {0}",
+                                 apm.UsersErorNotInLog.Count);
+            report.AppendFormat("\nUsersErorInLog Пользователей с ошибками в логе: {0}", apm.UsersErorInLog.Count);
+            report.AppendFormat("\nUsersErorInLogNotInADПользователей которых нет в AD но есть логи (уволенные или типа того): {0}",
+                                 apm.UsersErorInLogNotInAD.Count);
+            report.AppendFormat("\nUsersErorLongSuccessПользователей у которых давно не выполнялась настройка (может комп сменился или типа того): {0}",
+                                 apm.UsersErorLongSuccess.Count);
+            report.AppendLine("\nПодробнее:");
+            report.Append(getReportForUsers(apm.UsersErorNotInLog, "UsersErorNotInLog - которых нет в логах (не произошла настройка вообще)\n"));
+            report.Append(getReportForUsers(apm.UsersErorInLog, "UsersErorInLog Пользователей с ошибками в логе\n"));
+            report.Append(getReportForUsers(apm.UsersErorInLogNotInAD, "UsersErorInLogNotInADПользователей которых нет в AD но есть логи (уволенные или типа того)\n"));
+            report.Append(getReportForUsers(apm.UsersErorLongSuccess, "UsersErorLongSuccessПользователей у которых давно не выполнялась настройка (может комп сменился или типа того)\n"));
 
-      private static string getReportForUsers(List<UserInfo> users, string title)
-      {
-         StringBuilder res = new StringBuilder(title);
+            return report.ToString();
+        }
 
-         int i = 1;
-         foreach (var user in users)
-         {
-            res.AppendFormat("{0} {1}\n", i++, user.ToString());
-         }
+        private static string getReportForUsers(List<UserInfo> users, string title)
+        {
+            StringBuilder res = new StringBuilder(title);
 
-         return res.ToString();
-      }
-   }
+            int i = 1;
+            foreach (var user in users)
+            {
+                res.AppendFormat("{0} {1}\n", i++, user.ToString());
+            }
+
+            return res.ToString();
+        }
+    }
 }
