9ffd1c85bdbfc5598a45c7674ad3a59dd4a32e95
Vildar <vildar82@gmail.com>
2017-04-25 08:16:45 +0300

    IsAnyProcess

--- LogMonitor/Program.cs
+++ LogMonitor/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Diagnostics;
 using System.Windows.Forms;
 
 namespace LogMonitor
@@ -10,6 +12,10 @@ namespace LogMonitor
         [STAThread]
         private static void Main ()
         {
+            if (IsAnyProcess())
+            {
+                return;
+            }
             // Периодически считывать логи из
             // \\dsk2.picompany.ru\project\CAD_Settings\AutoCAD_server\ShareSettings\AutoCAD_PIK_Manager\Logs
             // Собирать логи от плагинов (Plugin).
@@ -17,6 +23,11 @@ namespace LogMonitor
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FormLog());
+        }
+
+        private static bool IsAnyProcess()
+        {
+            return Process.GetProcessesByName(Process.GetCurrentProcess().ProcessName).Skip(1).Any();
         }
     }
 }
